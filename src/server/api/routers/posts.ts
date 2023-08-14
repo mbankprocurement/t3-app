73b0f19f5667c0ced975cbac58680594928b27b1
Nyambayar1231 <nyambayarlucky@gmail.com>
2023-08-14 23:33:40 +0800

    Posting loading states + toast

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -73,13 +73,13 @@ export const postsRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().min(1).max(280),
+        content: z.string().emoji("only emojis are allowed!").min(1).max(280),
       })
     )
     .mutation(async ({ ctx, input }) => {
       const authorId = ctx.userId;
 
-      const { success } = await ratelimit.consume(authorId);
+      const { success } = await ratelimit.limit(authorId);
       if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
 
       const post = await ctx.prisma.post.create({
